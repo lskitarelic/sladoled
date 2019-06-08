98121c99c2097630286e66e4cb4b3916ec773f79
SladoledMaster <andrej.gardijan@gmail.com>
2019-06-08 19:33:22 +0200

    Modified quit screen.

--- collectible.py
+++ collectible.py
@@ -16,7 +16,7 @@ class Collectible(pygame.sprite.Sprite):
         #self.image.fill(color)
         # of rect.x and rect.y
         self.rect = self.image.get_rect()
-        self.rect.y = random.randrange(-100, -20)
+        self.rect.y = random.randrange(-300, -50)
         self.rect.x = random.randrange(0, screen_width)
         self.flag = True
 
@@ -25,7 +25,7 @@ class Collectible(pygame.sprite.Sprite):
         """ Reset position to the top of the screen, at a random x location.
         Called by update() or the main program loop if there is a collision.
         """
-        self.rect.y = random.randrange(-500, -20)
+        self.rect.y = random.randrange(-700, -50)
         self.rect.x = random.randrange(0, screen_width)
  
     def update(self, screen_width, score):
