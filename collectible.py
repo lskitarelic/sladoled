8f6f1dbe299715676859b3931ee45e1cde0985d8
SladoledMaster <andrej.gardijan@gmail.com>
2019-06-08 13:05:17 +0200

    Added fullscreen, ESC exit.

--- collectible.py
+++ collectible.py
@@ -2,33 +2,35 @@ import pygame
 import random
 
 class Collectible(pygame.sprite.Sprite):
-    """
-    This class represents the ball
-    It derives from the "Sprite" class in Pygame
-    """
     def __init__(self, color, width, height, image, screen_width):
         # Call the parent class (Sprite) constructor
         super().__init__()
- 
+        self.increment = 1
+
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
         self.image = image
         #self.image.fill(color)
         # of rect.x and rect.y
         self.rect = self.image.get_rect()
+        self.rect.y = random.randrange(-1000, -50)
+        self.rect.x = random.randrange(0, screen_width)
+
  
     def reset_pos(self, screen_width):
         """ Reset position to the top of the screen, at a random x location.
         Called by update() or the main program loop if there is a collision.
         """
-        self.rect.y = random.randrange(-300, -20)
+        self.rect.y = random.randrange(-200, -80)
         self.rect.x = random.randrange(0, screen_width)
  
-    def update(self, screen_width):
+    def update(self, screen_width, score):
         """ Called each frame. """
+        if score % 50 == 0 and self.increment <= 7:
+            self.increment += 0.2
  
         # Move block down one pixel
-        self.rect.y += 1
+        self.rect.y += self.increment
  
         # If block is too far down, reset to top of screen.
         if self.rect.y > 540:
--- space_game.py
+++ space_game.py
@@ -4,6 +4,15 @@ import player
 import collectible
 import os.path
 
+def add_sprites(block_num, block_list, all_sprites_list):
+    for i in range(block_num):
+        # This represents a block
+        block = collectible.Collectible(BLACK, 20, 15, ICE_CREAM, screen_width)
+     
+        # Add the block to the list of objects
+        block_list.add(block)
+        all_sprites_list.add(block)
+
  
 # Define some colors
 BLACK = (0, 0, 0)
@@ -17,10 +26,12 @@ SCOREBOARD_COLOR = (237, 252, 251)
 # Initialize Pygame
 pygame.init()
  
+pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
+
 # Set the height and width of the screen
 screen_width = 960
 screen_height = 540
-screen = pygame.display.set_mode([screen_width, screen_height])
+screen = pygame.display.set_mode([screen_width, screen_height], pygame.FULLSCREEN)
  
 DOGGO = pygame.image.load(os.path.join('images', 'doggo.png')).convert_alpha()
 ICE_CREAM = pygame.image.load(os.path.join('images', 'ice-cream.png')).convert_alpha()
@@ -33,18 +44,9 @@ block_list = pygame.sprite.Group()
 # This is a list of every sprite. All blocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
  
-block_num = 100
-for i in range(block_num):
-    # This represents a block
-    block = collectible.Collectible(BLACK, 20, 15, ICE_CREAM, screen_width)
- 
-    # Set a random location for the block
-    block.rect.x = random.randrange(screen_width)
-    block.rect.y = random.randrange(screen_height)
- 
-    # Add the block to the list of objects
-    block_list.add(block)
-    all_sprites_list.add(block)
+block_num = 1
+
+add_sprites(block_num, block_list, all_sprites_list)
  
 player = player.Player(RED, 20, 15, DOGGO, screen_width)
 all_sprites_list.add(player)
@@ -53,7 +55,6 @@ all_sprites_list.add(player)
 pygame.font.init()
 myfont = pygame.font.SysFont('Fira Mono Medium', 30)
  
-
 # Loop until the user clicks the close button.
 done = False
  
@@ -61,19 +62,23 @@ done = False
 clock = pygame.time.Clock()
  
 score = 0
- 
+textsurface = myfont.render("Score: " + str(score), False, SCOREBOARD_COLOR)
+
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
- 
+        elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+
     # Clear the screen
     screen.blit(background, (0, 0))
     
  
     # Calls update() method on every sprite in the list
-    all_sprites_list.update(screen_width)
+    all_sprites_list.update(screen_width, score)
  
     # See if the player block has collided with anything.
     blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
@@ -81,10 +86,11 @@ while not done:
     # Check the list of collisions.
     for block in blocks_hit_list:
         score += 1
-        print(score)
         textsurface = myfont.render("Score: " + str(score), False, SCOREBOARD_COLOR)
         # Reset block to the top of the screen to fall again.
         block.reset_pos(screen_width)
+        if score <= 15:
+            add_sprites(1, block_list, all_sprites_list)
  
     # Draw all the spites
     all_sprites_list.draw(screen)
@@ -93,8 +99,8 @@ while not done:
     screen.blit(textsurface, (0, 0))
     
  
-    # Limit to 20 frames per second
-    clock.tick(30)
+    # Limit to 60 frames per second
+    clock.tick(60)
  
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
